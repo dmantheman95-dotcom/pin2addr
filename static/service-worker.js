bab5c7a2be3e1658348606d52fdc11846b969e9c
dmantheman95-dotcom <dmantheman95@gmail.com>
2025-11-11 12:58:12 -0500

    Create service-worker.js

---
+++ static/service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "pinlookup-v1";
+const urlsToCache = [
+  "/",
+  "/static/style.css",
+  "/static/manifest.json",
+  "/static/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
